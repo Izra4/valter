ec03bee32da471427c1921d330e2234adc015204
izra4 <aryaizra2@gmail.com>
2023-10-22 21:57:29 +0700

    fix email

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	db2 "Valter/db"
 	"Valter/handler"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	//err := godotenv.Load()
-	//if err != nil {
-	//	panic(err)
-	//}
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
 	db, err := db2.InitDB()
 	if err != nil {
 		panic(err)
--- utility/util.go
+++ utility/util.go
@@ -74,117 +74,214 @@ func SendMailsBook(email, address, tanggal, due, invoiceNumber, description, nam
 	m.SetHeader("To", email)
 	m.SetHeader("Subject", "Booking Receipt")
 
+	//	body := `<!DOCTYPE html>
+	//<html lang="en">
+	//  <head>
+	//    <meta charset="UTF-8" />
+	//    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	//    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+	//    <title>Document</title>
+	//  </head>
+	//  <body class="p-8 space-y-12" style="padding: 29px;">
+	//    <div class="flexBetween" style="display: flex; justify-content: space-between;">
+	//      <h1 class="font-bold" style="font-weight: bold;">GIME</h1>
+	//      <div class="flexColumn" style="display: flex; flex-direction: column;">
+	//        <span>REG: 123000123000</span>
+	//        <span>Gime@aitech.co | +6213123213123</span>
+	//      </div>
+	//    </div>
+	//
+	//    <section class="flexBetween" style="display: flex; justify-content: space-between;">
+	//      <div>
+	//        <h2 class="font-bold" style="font-weight: bold;">` + name + `</h2>
+	//        <div class="flexColumn" style="display: flex; flex-direction: column; width: 30vw;">
+	//          <div class="flexBetween" style="display: flex; justify-content: space-between;">
+	//            <span>INVOICE NUMBER</span>
+	//            <span>` + invoiceNumber + `</span>
+	//          </div>
+	//          <div class="flexBetween" style="display: flex; justify-content: space-between;">
+	//            <span>INVOICE DATE</span>
+	//            <span>` + tanggal + `</span>
+	//          </div>
+	//          <div class="flexBetween" style="display: flex; justify-content: space-between;">
+	//            <span>DUE</span>
+	//            <span>` + due + `</span>
+	//          </div>
+	//        </div>
+	//      </div>
+	//
+	//      <div>
+	//        <h2 class="font-bold" style="font-weight: bold;">Address</h2>
+	//        <div class="flexColumn" style="display: flex; flex-direction: column;">
+	//          <span style="width: 20vw;">` + address + `</span>
+	//        </div>
+	//      </div>
+	//    </section>
+	//
+	//    <section>
+	//      <table class="w-full" style="width: 100%; margin: 24px 0; border-collapse: collapse;">
+	//        <tr>
+	//          <th style="border: 1px solid;">Description</th>
+	//          <th style="border: 1px solid;">Qty</th>
+	//          <th style="border: 1px solid;">Price</th>
+	//          <th style="border: 1px solid;">GST</th>
+	//          <th style="border: 1px solid;">Amount</th>
+	//        </tr>
+	//
+	//        <tr>
+	//          <td style="border: 1px solid;">` + description + `</td>
+	//          <td style="border: 1px solid;">1</td>
+	//          <td style="border: 1px solid;">$5,455.00</td>
+	//          <td style="border: 1px solid;">$545.50</td>
+	//          <td style="border: 1px solid;">$6,000.50</td>
+	//        </tr>
+	//
+	//      </table>
+	//    </section>
+	//
+	//    <section class="w-full" style="width: 100%; margin: 24px 0; display: flex; justify-content: flex-end;">
+	//      <div
+	//        class="bg-neutral-100 p-4" style="background-color: #F9F9FA; border: 2px solid #DFE4EA; padding: 16px; width: 50vw; border-radius: 15px;"
+	//      >
+	//        <div class="flexBetween" style="display: flex; justify-content: space-between;">
+	//          <span>Sub total (excl. GST)</span>
+	//          <span>$5,455.00</span>
+	//        </div>
+	//        <div class="flexBetween" style="display: flex; justify-content: space-between;">
+	//          <span>Total GST:</span>
+	//          <span>$545.50</span>
+	//        </div>
+	//        <div class="flexBetween" style="display: flex; justify-content: space-between;">
+	//          <span>Credit card fee (if using):</span>
+	//          <span>$25.00</span>
+	//        </div>
+	//        <div class="pt-4 font-bold flexBetween" style="display: flex; justify-content: space-between; font-weight: bold;">
+	//          <p>Amount due on ` + due + `:</p>
+	//          <p>6,025.50 NZD</p>
+	//        </div>
+	//      </div>
+	//    </section>
+	//
+	//    <div class="flexBetween" style="display: flex; justify-content: space-between;">
+	//      <div class="space-y-6">
+	//        <h2 class="font-bold" style="font-weight: bold;">PAYMENT INSTRUCTION</h2>
+	//
+	//        <div class="flexColumn" style="display: flex; flex-direction: column; list-style: none; text-indent: 0;">
+	//          <span>Bank name: ABC Bank</span>
+	//          <span>Account number: 12-1234-123456-12</span>
+	//          <span class="font-bold" style="font-weight: bold;">
+	//            Please use as ` + invoiceNumber + ` as a reference number
+	//          </span>
+	//        </div>
+	//
+	//        <p>For any questions please contact us at Gime@aitech.co</p>
+	//      </div>
+	//
+	//      <div class="flexColumn" style="display: flex; flex-direction: column;">
+	//        <span>Pay Online</span>
+	//        <a href="https://buy.stripe.com/" class="underline font-bold" style="text-decoration: underline; font-weight: bold;">https://buy.stripe.com/</a>
+	//      </div>
+	//    </div>
+	//  </body>
+	//</html>`
 	body := `<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
-    <title>Document</title>
-  </head>
-  <body class="p-8 space-y-12" style="padding: 29px;">
-    <div class="flexBetween" style="display: flex; justify-content: space-between;">
-      <h1 class="font-bold" style="font-weight: bold;">GIME</h1>
-      <div class="flexColumn" style="display: flex; flex-direction: column;">
-        <span>REG: 123000123000</span>
-        <span>Gime@aitech.co | +6213123213123</span>
+<html>
+<head>
+  <style>
+    table {
+      border-collapse: collapse;
+      width: 100%;
+    }
+    table, th, td {
+      border: 1px solid black;
+    }
+  </style>
+</head>
+<body>
+  <div style="border: 1px solid black; min-width: 800px;">
+    <div style="display: flex; justify-content: space-between;">
+      <h1 style="font-weight: bold; margin: 0;">GIME</h1>
+      <div style="display: flex; flex-direction: column;">
+        <p>REG: 123000123000</p>
+        <p>Gime@aitech.co | +6213123213123</p>
       </div>
     </div>
-
-    <section class="flexBetween" style="display: flex; justify-content: space-between;">
+    <section style="display: flex; justify-content: space-between;">
       <div>
-        <h2 class="font-bold" style="font-weight: bold;">` + name + `</h2>
-        <div class="flexColumn" style="display: flex; flex-direction: column; width: 30vw;">
-          <div class="flexBetween" style="display: flex; justify-content: space-between;">
-            <span>INVOICE NUMBER</span>
-            <span>` + invoiceNumber + `</span>
-          </div>
-          <div class="flexBetween" style="display: flex; justify-content: space-between;">
-            <span>INVOICE DATE</span>
-            <span>` + tanggal + `</span>
-          </div>
-          <div class="flexBetween" style="display: flex; justify-content: space-between;">
-            <span>DUE</span>
-            <span>` + due + `</span>
-          </div>
+        <h2 style="font-weight: bold;">` + name + `</h2>
+        <div style="display: flex; flex-direction: column; width: 30vw;">
+          <div><strong>INVOICE NUMBER:</strong>` + invoiceNumber + `</div>
+          <div><strong>INVOICE DATE:</strong>` + tanggal + `</div>
+          <div><strong>DUE:</strong>` + due + `</div>
         </div>
       </div>
-
       <div>
-        <h2 class="font-bold" style="font-weight: bold;">Address</h2>
-        <div class="flexColumn" style="display: flex; flex-direction: column;">
+        <h2 style="font-weight: bold;">Address</h2>
+        <div style="display: flex; flex-direction: column;">
           <span style="width: 20vw;">` + address + `</span>
         </div>
       </div>
     </section>
-
     <section>
-      <table class="w-full" style="width: 100%; margin: 24px 0; border-collapse: collapse;">
-        <tr>
-          <th style="border: 1px solid;">Description</th>
-          <th style="border: 1px solid;">Qty</th>
-          <th style="border: 1px solid;">Price</th>
-          <th style="border: 1px solid;">GST</th>
-          <th style="border: 1px solid;">Amount</th>
-        </tr>
-
-        <tr>
-          <td style="border: 1px solid;">` + description + `</td>
-          <td style="border: 1px solid;">1</td>
-          <td style="border: 1px solid;">$5,455.00</td>
-          <td style="border: 1px solid;">$545.50</td>
-          <td style="border: 1px solid;">$6,000.50</td>
-        </tr>
-        
+      <table>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Qty</th>
+            <th>Price</th>
+            <th>GST</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>` + description + `</td>
+            <td>1</td>
+            <td>$5,455.00</td>
+            <td>$545.50</td>
+            <td>$6,000.50</td>
+          </tr>
+        </tbody>
       </table>
     </section>
-
-    <section class="w-full" style="width: 100%; margin: 24px 0; display: flex; justify-content: flex-end;">
-      <div
-        class="bg-neutral-100 p-4" style="background-color: #F9F9FA; border: 2px solid #DFE4EA; padding: 16px; width: 50vw; border-radius: 15px;"
-      >
-        <div class="flexBetween" style="display: flex; justify-content: space-between;">
-          <span>Sub total (excl. GST)</span>
-          <span>$5,455.00</span>
+    <section style="width: 100%; margin: 24px 0;">
+      <div style="background-color: #f9f9fa; border: 2px solid #dfe4ea; padding: 16px; border-radius: 15px;">
+        <div style="display: flex; justify-content: space-between;">
+          <div>Sub total (excl. GST):</div>
+          <div>$5,455.00</div>
         </div>
-        <div class="flexBetween" style="display: flex; justify-content: space-between;">
-          <span>Total GST:</span>
-          <span>$545.50</span>
+        <div style="display: flex; justify-content: space-between">
+          <div>Total GST:</div>
+          <div>$545.50</div>
         </div>
-        <div class="flexBetween" style="display: flex; justify-content: space-between;">
-          <span>Credit card fee (if using):</span>
-          <span>$25.00</span>
+        <div style="display: flex; justify-content: space-between">
+          <div>Credit card fee (if using):</div>
+          <div>$25.00</div>
         </div>
-        <div class="pt-4 font-bold flexBetween" style="display: flex; justify-content: space-between; font-weight: bold;">
-          <p>Amount due on ` + due + `:</p>
-          <p>6,025.50 NZD</p>
+        <div style="display: flex; justify-content: space-between; font-weight: bold;">
+          <div>Amount due on ` + due + `:</div>
+          <div>6,025.50 NZD</div>
         </div>
       </div>
     </section>
-
-    <div class="flexBetween" style="display: flex; justify-content: space-between;">
-      <div class="space-y-6">
-        <h2 class="font-bold" style="font-weight: bold;">PAYMENT INSTRUCTION</h2>
-
-        <div class="flexColumn" style="display: flex; flex-direction: column; list-style: none; text-indent: 0;">
-          <span>Bank name: ABC Bank</span>
-          <span>Account number: 12-1234-123456-12</span>
-          <span class="font-bold" style="font-weight: bold;">
-            Please use as ` + invoiceNumber + ` as a reference number
-          </span>
+    <div style="display: flex; justify-content: space-between;">
+      <div style="display: flex; flex-direction: column;">
+        <h2 style="font-weight: bold;">PAYMENT INSTRUCTION</h2>
+        <div style="display: flex; flex-direction: column;">
+          <p>Bank name: ABC Bank</p>
+          <p>Account number: 12-1234-123456-12</p>
+          <p><strong>Please use ` + invoiceNumber + ` as a reference number</strong></p>
         </div>
-
         <p>For any questions please contact us at Gime@aitech.co</p>
       </div>
-
-      <div class="flexColumn" style="display: flex; flex-direction: column;">
-        <span>Pay Online</span>
-        <a href="https://buy.stripe.com/" class="underline font-bold" style="text-decoration: underline; font-weight: bold;">https://buy.stripe.com/</a>
+      <div style="display: flex; flex-direction: column;">
+        <p>Pay Online</p>
+        <a href="https://buy.stripe.com/" style="text-decoration: underline; font-weight: bold;">https://buy.stripe.com/</a>
       </div>
     </div>
-  </body>
+  </div>
+</body>
 </html>`
-
 	m.SetBody("text/html", body)
 
 	d := gomail.NewDialer("smtp.gmail.com", 587, "aryaizra2@gmail.com", os.Getenv("PASS"))
